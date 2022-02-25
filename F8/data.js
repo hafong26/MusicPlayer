3669677d53063bed68df14d60b048f17e471c521
Nguyễn Văn Hào <85483297+hafong26@users.noreply.github.com>
2022-02-25 14:21:36 +0700

    Update data.js

--- F8/data.js
+++ F8/data.js
@@ -1,64 +1,64 @@
 var songs = [{
         "name": "Đa Tình",
         "singer": "Quinn x Phong DN",
-        "src": "https://github.com/hafong26/library/blob/main/audio/F8_Music/remixDaTinh.mp3?raw=true",
-        "img": "https://github.com/hafong26/library/blob/main/images/F8_Music/DaTinh.webp?raw=true",
+        "src": "music/remixDaTinh.mp3?raw=true",
+        "img": "./img/DaTinh.webp?raw=true",
         "id": 1
     },
     {
         "name": "Đêm Định Mệnh",
         "singer": "Minh Lý Remix",
-        "src": "https://github.com/hafong26/library/blob/main/audio/F8_Music/remixDemDinhMenh.mp3?raw=true",
-        "img": "https://github.com/hafong26/library/blob/main/images/F8_Music/DemDinhMenh.webp?raw=true",
+        "src": "music/remixDemDinhMenh.mp3?raw=true",
+        "img": "./img/DemDinhMenh.webp?raw=true",
         "id": 2
     },
     {
         "name": "Em Là Hoàng Hôn",
         "singer": "Vang x Cloud 5",
-        "src": "https://github.com/hafong26/library/blob/main/audio/F8_Music/remixEmLaHoangHon.mp3?raw=true",
-        "img": "https://github.com/hafong26/library/blob/main/images/F8_Music/EmLaHoangHon.webp?raw=true",
+        "src": "music/remixEmLaHoangHon.mp3?raw=true",
+        "img": "./img/EmLaHoangHon.webp?raw=true",
         "id": 3
     },
     {
         "name": "Happy New Year",
         "singer": "ABBA",
-        "src": "https://github.com/hafong26/library/blob/main/audio/F8_Music/remixHappyNewYear.mp3?raw=true",
-        "img": "https://github.com/hafong26/library/blob/main/images/F8_Music/HappyNewYear.webp?raw=true",
+        "src": "music/remixHappyNewYear.mp3?raw=true",
+        "img": "./img/HappyNewYear.webp?raw=true",
         "id": 4
     },
     {
         "name": "In The End",
         "singer": "Linkin Park",
-        "src": "https://github.com/hafong26/library/blob/main/audio/F8_Music/remixInTheEnd.mp3?raw=true",
-        "img": "https://github.com/hafong26/library/blob/main/images/F8_Music/InTheEnd.webp?raw=true",
+        "src": "music/remixInTheEnd.mp3?raw=true",
+        "img": "./img/InTheEnd.webp?raw=true",
         "id": 5
     },
     {
         "name": "Khi Anh Qua Thung Lũng",
         "singer": "Đạt G",
-        "src": "https://github.com/hafong26/library/blob/main/audio/F8_Music/remixKhiAnhQuaThungLung.mp3?raw=true",
-        "img": "https://github.com/hafong26/library/blob/main/images/F8_Music/KhiAnhQuaThungLung.webp?raw=true",
+        "src": "music/remixKhiAnhQuaThungLung.mp3?raw=true",
+        "img": "./img/KhiAnhQuaThungLung.webp?raw=true",
         "id": 6
     },
     {
         "name": "Quên Một Người Từng Yêu",
         "singer": "Tú Na Cover",
-        "src": "https://github.com/hafong26/library/blob/main/audio/F8_Music/remixQuenMotNguoiTungYeu.mp3?raw=true",
-        "img": "https://github.com/hafong26/library/blob/main/images/F8_Music/QuenMotNguoiTungYeu.webp?raw=true",
+        "src": "music/remixQuenMotNguoiTungYeu.mp3?raw=true",
+        "img": "./img/QuenMotNguoiTungYeu.webp?raw=true",
         "id": 7
     },
     {
         "name": "Xem Như Em Chẳng May",
         "singer": "Chu Thuý Quỳnh",
-        "src": "https://github.com/hafong26/library/blob/main/audio/F8_Music/remixXemNhuEmChangMay.mp3?raw=true",
-        "img": "https://github.com/hafong26/library/blob/main/images/F8_Music/XemNhuEmChangMay.webp?raw=true",
+        "src": "music/remixXemNhuEmChangMay.mp3?raw=true",
+        "img": "./img/XemNhuEmChangMay.webp?raw=true",
         "id": 8
     },
     {
         "name": "Yêu Đừng Sợ Đau",
         "singer": "Ngô Lan Hương",
-        "src": "https://github.com/hafong26/library/blob/main/audio/F8_Music/remixYeuDungSoDau.mp3?raw=true",
-        "img": "https://github.com/hafong26/library/blob/main/images/F8_Music/YeuDungSoDau.webp?raw=true",
+        "src": "music/remixYeuDungSoDau.mp3?raw=true",
+        "img": "./img/YeuDungSoDau.webp?raw=true",
         "id": 9
     }
 ]
